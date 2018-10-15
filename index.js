024768fc7f1673b9f1a105c916d52bdcee22c2b0
Shakarra McGuire <shakarramcguire@gmail.com>
2018-10-15 23:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+// drivers.push('Ralph');
+// drivers;
+
+// drivers.unshift('Bob');
+// drivers.pop();
+// drivers.shift();
+// allDrivers = [...drivers, 'Broom'];
+// allDrivers = ['Arnold', ...drivers];
+// drivers.slice(-1);
+// drivers.slice(1);
