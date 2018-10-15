e04074a1b132f2ad9b996f1df51200382f646674
Shakarra McGuire <shakarramcguire@gmail.com>
2018-10-15 23:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
-function destructivelyAppend(name) {
+function destructivelyAppendDriver(name) {
   drivers.push('name');
 }
 
